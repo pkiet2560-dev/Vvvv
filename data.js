511c7a0814048419f1fbff3c934651beb58c07dc
pkiet2560-dev <pkiet2560@gmail.com>
2026-04-19 20:48:34 +0700

    Update and rename Index.html to data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const OMNI_DATABASE = {
+    aliens: [
+        { id: 1, name: "Heatblast", series: "Classic", species: "Pyronite" },
+        { id: 2, name: "Feedback", series: "Omniverse", species: "Conductoid" },
+        { id: 3, name: "Blood-Chill", series: "Carnitrix", species: "Necrofriggian (Mutant)" } // Fan-made
+    ],
+    watches: [
+        { name: "Omnitrix", color: "#00ff00", user: "Ben Prime" },
+        { name: "Albedo Ultimatrix", color: "#ff0000", user: "Albedo" },
+        { name: "Carnitrix", color: "#4a0000", user: "Classic Ben (AU)" }
+    ]
+};
